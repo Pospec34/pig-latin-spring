754ebeb5b68f2bbcc6b3a207723972a5d062188e
Pospec34 <pospisilpetr631@gmail.com>
2023-11-29 11:23:18 +0100

    Updated capitalizeFirstLetter method and added method description.
    capitalizeFirstLetter method now capitalizes first letter of each sentence instead of only the first one

--- src/main/java/com/example/PigLatin/models/PigLatinConverter.java
+++ src/main/java/com/example/PigLatin/models/PigLatinConverter.java
@@ -3,6 +3,7 @@ package com.example.PigLatin.models;
 public class PigLatinConverter {
 
     public String convertToPigLatin(String text){
+        text = text.toLowerCase();
         String[] words = text.split(" ");
         String pigSentence = "";
 
@@ -20,26 +21,65 @@ public class PigLatinConverter {
     }
 
     /**
+     * Checks if the text starts with a vowel
      *
-     * @param text
-     * @return returns true if text starts with vowel
+     * @param text The input text to be checked
+     * @return returns true if text starts with vowel, otherwise returns false
      */
     public boolean startsWithVowel(String text){
         return text.matches("^[aeiouAEIOU].*");
     }
 
+
+    /**
+     * Swaps the consonants in a word according to Pig Latin rules
+     *
+     * @param text The input text to be changed
+     * @return returns the text with swapped consonants and added "ay" at the end
+     */
     public String swapConsonants(String text){
         String beginning = text.replaceAll(".*?([aeiouAEIOU].*)", "$1");
         String end = text.replaceAll("([^aeiouAEIOU]*).*", "$1");
         return beginning + end + "ay";
     }
 
-
+    /**
+     * This method splits the pig sentence into an array and then capitalizes the first letter of each sentence
+     * @param text The input text to be changed
+     * @return returns the same text it has received but capitalized
+     */
     public String capitalizeFirstLetter(String text){
-        text = text.substring(0, 1).toUpperCase() + text.substring(1);
-        return text;
+        boolean newSentence = true;
+        String result = "";
+        String words[] = text.split(" ");
+
+        for (String word : words){
+            if (newSentence){
+                result += word.substring(0, 1).toUpperCase() + word.substring(1) + " ";
+            } else {
+                result += word + " ";
+            }
+            newSentence = isNewSentence(word);
+        }
+
+        return result.trim();
     }
 
+    /**
+     * Checks if the word ends with a punctuation mark indicating a new sentence
+     * @param word The input word to be checked
+     * @return returns true if the word ends with either question mark, exclamation mark or full stop
+     */
+    public boolean isNewSentence(String word){
+        return word.matches(".*[.!?]+$");
+    }
+
+
+    /**
+     * Retrieves the punctuation marks (.,?!) at the end of the text.
+     * @param text The input text to be checked for punctuation
+     * @return returns either the punctuation marks at the end of the text or an empty string
+     */
     public String getPunctuation(String text){
         if (text.matches(".*[.,!?]+$")){
             return text.replaceAll(".*([.,!?]+$)", "$1");
